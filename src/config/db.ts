bf43691e47341c1a787a3a9a929c2405488f161b
GIGLIARLY MARCELINO GONZAGA <gigliarly.gonzaga@maisunifacisa.com.br>
2025-03-27 10:41:53 -0300

    adicao forma de conexao online porem apresenta erro na leitura do arquivo .env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,25 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
-import { env } from "../env";
+import { envTranformed } from "../env";
 
-mongoose.connect(env.MONGO_URI, {
+mongoose.connect(envTranformed.MONGO_URI, {
     socketTimeoutMS: 30000,
 })
     .then(() => console.log('Conectado ao MongoDB'))
-    .catch(err => console.error(`Erro ao conectar ao MongoDB: ${err}`))
+    .catch(() => {
+        console.error("Erro ao estabeler conexao local")
+        console.log("Continuando com conexao online")
+        const client = new MongoClient(envTranformed.MONGO_URI_ONLINE, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true
+            }
+        })
+
+        client.connect().then(({ db }) => {
+            db("Traits").command({ ping: 1 })
+            console.log("Pinged your deployment. You successfully")
+        })
+        .catch(() => console.log("Erro ao conectar com o mongodb online"))
+    })
--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,13 @@
 import z from "zod";
 import dotenv from "dotenv"
-
 dotenv.config()
+import { env } from "node:process";
 
 const envTransform = z.object({
     PORT: z.coerce.number().default(8080),
     SECRET: z.string(),
-    MONGO_URI: z.string()
+    MONGO_URI: z.string(),
+    MONGO_URI_ONLINE: z.string()
 })
 
-export const env = envTransform.parse(process.env)
+export const envTranformed = envTransform.parse(env)
