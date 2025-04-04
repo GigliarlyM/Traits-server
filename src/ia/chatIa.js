a59b66f794817604c93e0cf2bd0659aee90a6318
Luiz Neto <138041202+CtrlNeto@users.noreply.github.com>
2025-04-04 13:45:04 -0300

    Mudança do stdin e chamada do método
    Vamos usar isso em um chat, não em um terminal

--- src/ia/chatIa.js
+++ src/ia/chatIa.js
@@ -7,8 +7,13 @@ dotenv.config();
 
 // Esse startChat consiste na tentativa inicial de ser integrado ao gifted-chat, que pode ser mudado depois
 async function startChat(prompt, history = []) {
+
+    if (prompt.toLowerCase() === "fim") {
+        return "Chat encerrado";
+    }
+
     const apiKey = process.env.GOOGLE_GEMINI_API_KEY;
-    
+
     if (!apiKey) {
         throw new Error("Chave de API não encontrada. Verifique o arquivo .env ou as variáveis de ambiente.");
     }
@@ -17,26 +22,13 @@ async function startChat(prompt, history = []) {
     const model = genai.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
     const chat = model.startChat({ history });
 
-
-    console.log("\nChat iniciado. Digite 'fim' para encerrar.\n");
-
-    process.stdin.on("data", async (data) => {
-        const prompt = data.toString().trim();
-        if (prompt.toLowerCase() === "fim") {
-            console.log("Encerrando a conversa.");
-            process.exit();
-        }
-
-        try {
-            const result = await chat.sendMessage(prompt);
-            const responseText = result.response.text();  
-            console.log(`Resposta: ${responseText}`);
-        } catch (error) {
-            console.error("Erro ao obter resposta:", error);
-        }
-    });
+    try {
+        const result = await chat.sendMessage(prompt);
+        const responseText = result.response.text;
+        console.log(`Resposta: ${responseText}`);
+    } catch (error) {
+        console.error("Erro ao obter resposta:", error);
+    }
 }
-
-startChat();
 
 module.exports = { startChat };
