789155a9d20839f21a674f55e24c73fd7fd26e2b
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-04-05 19:48:37 -0300

    adicionado conversa com ia, porem nao esta testado

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -1,6 +1,7 @@
 import websocketPlugin from "@fastify/websocket";
 import { FastifyInstance } from "fastify";
 import { WebSocket } from "ws";
+import sendMessageToAi from "./ia/aiConnection";
 
 interface ChatMessage {
   type: 'message' | 'error' | 'join' | 'leave';
@@ -78,6 +79,27 @@ export async function webSocketRoutes(fastify: FastifyInstance) {
       if (activeConnections.has(to)) {
         activeConnections.get(to)?.send(messageString)
         activeConnections.get(message.user)?.send(messageString)
+      } else if (to == "chat-ia") {
+        sendMessageToAi(message.message).then((response) => {
+          if (response != null) {
+            const responseMessage: ChatMessage = {
+              type: 'message',
+              user: 'AI',
+              message: response(),
+              timestamp: new Date(),
+            }
+
+            activeConnections.get(message.user)?.send(JSON.stringify(responseMessage))
+          }else {
+            const errorMessage: ChatMessage = {
+              type: 'error',
+              message: 'Ocorreu um erro ao enviar a mensagem para o AI',
+              user: 'System',
+              timestamp: new Date(),
+            }
+            activeConnections.get(message.user)?.send(JSON.stringify(JSON.stringify(errorMessage)))
+          }
+        })
       }
 
       return
